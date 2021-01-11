746c992be35585afc872c7e26b00db3f5b45aa1f
Niloy <nilooy.dev@gmail.com>
2021-01-11 01:47:10 +0100

    conflict

--- imports/ui/src/components/shared/ThemedSuspense.jsx
+++ imports/ui/src/components/shared/ThemedSuspense.jsx
@@ -17,9 +17,9 @@ function ThemedSuspense() {
   }, []);
 
   return (
-    <div className="w-full h-screen flex justify-center items-center p-6 text-lg font-medium text-purple-700 dark:text-gray-200 dark:bg-gray-900">
+    <div className="w-full h-screen flex justify-center items-center p-6 text-lg font-medium text-blue-700 dark:text-gray-200 dark:bg-gray-900">
       <div className="w-32 h-32 flex justify-center items-center bg-purple-200 rounded-full animate-ping">
-        <GiMeditation className="text-purple-700 text-6xl" />
+        <GiMeditation className="text-blue-700 text-5xl animate-pulse" />
       </div>
     </div>
   );
