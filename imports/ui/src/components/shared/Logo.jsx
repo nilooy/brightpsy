bcad0918cc07dcc4561f7ba0ff25eb6675a40d3e
Niloy <nilooy.dev@gmail.com>
2021-01-11 23:43:24 +0100

    Change logo

--- imports/ui/src/components/shared/Logo.jsx
+++ imports/ui/src/components/shared/Logo.jsx
@@ -7,6 +7,15 @@ const Logo = ({ link }) => {
       className="flex ml-6 text-lg font-bold text-gray-800 dark:text-gray-200"
       href={link}
     >
+      <img src="/img/logo.png" className="w-32" />
+    </a>
+  );
+
+  return (
+    <a
+      className="flex ml-6 text-lg font-bold text-gray-800 dark:text-gray-200"
+      href={link}
+    >
       <GiMeditation className="text-blue-700 text-2xl" />
       Bright<span className="text-blue-700 text-xl">Psi</span>
     </a>
