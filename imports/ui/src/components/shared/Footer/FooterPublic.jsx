ac7bf7a0cfc9f0c3b638aa92ef555688e83ee2f9
shuvo <shuvorahamanmd@gmail.com>
2021-01-15 15:35:18 +0100

    psycology list footer fixed

--- imports/ui/src/components/shared/Footer/FooterPublic.jsx
+++ imports/ui/src/components/shared/Footer/FooterPublic.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function FooterPublic() {
   return (
     <>
-      <footer className="relative bg-gray-300 pt-8 pb-6">
+      <footer className="relative bg-gray-300 pt-8 pb-6 ">
         <div
           className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
           style={{ height: "80px", transform: "translateZ(0)" }}
