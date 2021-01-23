247f98e77e7fab3d039bf8cd35549d3e03c10bc0
Niloy <nilooy.dev@gmail.com>
2021-01-23 20:08:11 +0100

    Restructure whole project

---
+++ imports/@ui/routes/Routes.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import { Route, Switch } from "react-router-dom";
+import Page404 from "@ui/routes/Page404";
+import { privateRoutes, privateRoutesUser } from "./private";
+import auth from "./auth";
+import { SecurityContext } from "../context/SecurityContext";
+
+const Routes = () => {
+  const { isDoctor } = useContext(SecurityContext);
+  const privateRoutesBasedOnRole = isDoctor ? privateRoutes : privateRoutesUser;
+
+  const routes = [...privateRoutesBasedOnRole, ...auth];
+
+  return (
+    <>
+      <Switch>
+        {routes.map((route, i) => (
+          <Route
+            key={i}
+            exact={route.subRoutes.some((r) => r.exact)}
+            path={route.subRoutes.map((r) => r.path)}
+          >
+            <route.layout>
+              {route.subRoutes.map((subRoute, i) => (
+                <Route key={i} {...subRoute} />
+              ))}
+            </route.layout>
+          </Route>
+        ))}
+        <Route path="*">
+          <Page404 />
+        </Route>
+      </Switch>
+    </>
+  );
+};
+
+export default Routes;
--- imports/api/register-api.js
+++
@@ -1,5 +0,0 @@
-import "./services/auth";
-import "./services/pricePackages";
-import "./services/tags";
-import "./services/user";
-import "./services/chat";
