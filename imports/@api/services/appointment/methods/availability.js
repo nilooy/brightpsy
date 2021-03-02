a64c6431ee0887d9cc6d0a3b00926b7b88c75525
nilooy <nilooy.dev@gmail.com>
2021-03-02 11:28:49 +0100

    Add default availability

--- imports/@api/services/appointment/methods/availability.js
+++ imports/@api/services/appointment/methods/availability.js
@@ -1,5 +1,21 @@
+import { Availabilities } from "../model/AvailabilityCollection";
+import { defaultAvailabilityData } from "../model/defaultAvailabilities";
+
 Meteor.methods({
-  "availability.create"({ data }) {
-    console.log(data);
+  "availability.createDefault"({ data }) {
+    if (Availabilities.findOne({ userId: Meteor.userId() })) {
+      return Meteor.Error("User already have availability");
+    }
+
+    return Availabilities.insert({
+      ...defaultAvailabilityData,
+      userId: Meteor.userId(),
+    });
+  },
+
+  "availability.resetToDefault"({ data }) {
+    return Availabilities.update(data._id, {
+      $set: { ...defaultAvailabilityData, userId: Meteor.userId() },
+    });
   },
 });
